653795ae0388e5df6b46a613bcaa91f658b9bb5a
JollyMick <scarpa.michele.90@gmail.com>
2025-01-20 08:31:50 +0100

    create resume component

---
+++ src/components/Resume.svelte
@@ -0,0 +1,24 @@
+<div class="relative py-1 bg-white overflow-hidden">
+    <div class="relative px-4 sm:px-6 lg:px-8">
+        <div class="text-lg max-w-prose mx-auto">
+            <h1 >
+                <span class="block text-base text-center text-jm-600 font-semibold tracking-wide uppercase">
+                    Michele Scarpa
+                </span>
+                <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                    Software engineer
+                </span>
+            </h1>
+            <!-- <MainSkills />
+            <Contact /> -->
+        </div>
+        <div class="mt-3 prose prose-indigo prose-lg text-gray-500 mx-auto">
+            <!-- <Presentation />
+            <Skills />
+            <Experiences />
+            <Certifications />
+            <Studies /> -->
+        </div>
+    </div>
+
+</div>
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -20,6 +20,28 @@
 </svelte:head>
 
 <main>
+
+	<div class="relative bg-gray-100">
+
+        <div class="lg:absolute lg:inset-0">
+            <div class="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/3">
+                <img
+                    class="h-56 w-full object-cover lg:absolute lg:h-full"
+                    src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80"
+                    alt=""
+                />
+            </div>
+        </div>
+        <div class="relative pt-4 pb-16 px-4 sm:pt-4 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-4">
+            <div class="relative lg:col-start-2 lg:col-span-3 lg:pl-8 drop-shadow-md rounded-md p-2">
+                <!-- {DButton}
+                <div ref={componentRef} >
+                    <Resume />
+                </div> -->
+            </div>
+        </div>
+
+    </div>
 	<h1>{config?.name}</h1>
 	<h2>{config?.role}</h2>
 	<p>{config?.description}</p>
