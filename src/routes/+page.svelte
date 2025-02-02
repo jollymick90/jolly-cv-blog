fdffb1ce279ca22a248e8da15a239760b6bba9f7
JollyMick <scarpa.michele.90@gmail.com>
2025-02-02 19:46:14 +0100

    home detail part-1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import Container from '../components/Container.svelte';
+	import HomeArticleSection from '../components/home/HomeArticleSection.svelte';
 	import HomeHeadings from '../components/home/HomeHeadings.svelte';
+	import HomeNewsLetterSection from '../components/home/HomeNewsLetterSection.svelte';
+	import HomeResume from '../components/home/HomeResume.svelte';
 	
 	import Photos from '../components/home/Photos.svelte';
 
@@ -16,3 +19,17 @@
 	<HomeHeadings {data}></HomeHeadings>
 </Container>
 <Photos></Photos>
+
+<Container className="mt-24 md:mt-28">
+
+
+<div class="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
+          <div class="flex flex-col gap-16">
+            <HomeArticleSection></HomeArticleSection>
+          </div>
+          <div class="space-y-10 lg:pl-16 xl:pl-24">
+            <HomeNewsLetterSection></HomeNewsLetterSection>
+            <HomeResume></HomeResume>
+          </div>
+        </div>
+</Container>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,15 +1,19 @@
 <script>
+	import Container from '../../components/Container.svelte';
+
 	export let data;
 	let { posts } = data;
 </script>
 
+<Container className="mt-9">
 <h1>Blog</h1>
 
 {#each posts as post}
-	<article>
+	<article class="text-white">
 		<h2>{post.metadata.title}</h2>
 		<p>{post.metadata.date}</p>
 		<!-- post.component è il contenuto markdown -->
 		<svelte:component this={post.component} />
 	</article>
 {/each}
+</Container>
