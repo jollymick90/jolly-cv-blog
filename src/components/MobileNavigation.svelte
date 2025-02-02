527eb7d34e5095bd2715fcd82af4d6bfa616b7d0
scardummichele <scarpa.michele.90@gmail.com>
2025-02-02 05:31:44 +0100

    Navigation Components

--- src/components/MobileNavigation.svelte
+++ src/components/MobileNavigation.svelte
@@ -1,48 +1,110 @@
-<script>
-    let isOpen = false;
-    
-    function toggleMenu() {
-      isOpen = !isOpen;
-    }
-  </script>
-  
- 
-    <button 
-      type="button" 
-      class="inline-flex items-center gap-x-1 text-sm font-semibold text-gray-900" 
-      aria-expanded={isOpen} 
-      on:click={toggleMenu}
-    >
-      <span>Solutions</span>
-      <svg class="size-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-        <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
-      </svg>
-    </button>
-    <div class="relative">
-    {#if isOpen}
-      <div class="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
-        <div class="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm shadow-lg ring-1 ring-gray-900/5">
-          <div class="p-4">
-            {#each [
-              { title: 'Analytics', description: 'Get a better understanding of your traffic', icon: 'M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z' },
-              { title: 'Engagement', description: 'Speak directly to your customers', icon: 'M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59' },
-              { title: 'Security', description: 'Your customers\' data will be safe and secure', icon: 'M7.864 4.243A7.5 7.5 0 0 1 19.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 0 0 4.5 10.5a7.464 7.464 0 0 1-1.15 3.993m1.989 3.559A11.209 11.209 0 0 0 8.25 10.5a3.75 3.75 0 1 1 7.5 0c0 .527-.021 1.049-.064 1.565' }
-            ] as item}
-              <div class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-                <div class="mt-1 flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                  <svg class="size-6 text-gray-600 group-hover:text-indigo-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" d={item.icon} />
-                  </svg>
-                </div>
-                <div>
-                  <a href="#" class="font-semibold text-gray-900">{item.title}</a>
-                  <p class="mt-1 text-gray-600">{item.description}</p>
-                </div>
-              </div>
-            {/each}
-          </div>
-        </div>
-      </div>
-    {/if}
-  </div>
-  
+<script context="module" lang="ts">
+export type ItemList = {
+		title: string;
+		description: string;
+		icon: string;
+        href: string;
+	};  </script>
+<script lang="ts">
+	let showIcon = false;
+	export let itemsList: ItemList[] = [];
+	let isOpen = false;
+
+	function toggleMenu() {
+		isOpen = !isOpen;
+	}
+</script>
+
+<button
+	type="button"
+	class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 
+    dark:bg-zinc-800/90 
+    hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+	aria-controls="mobile-menu"
+	aria-expanded={isOpen}
+	on:click={toggleMenu}
+>
+	<span class="absolute -inset-0.5"></span>
+	<span class="sr-only">Open main menu</span>
+
+	{#if isOpen}
+		<!--
+        Icon when menu is open.
+
+        Menu open: "block", Menu closed: "hidden"
+        -->
+		<svg
+			class="size-6"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			aria-hidden="true"
+			data-slot="icon"
+		>
+			<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+		</svg>
+	{:else}
+		<!--
+        Icon when menu is closed.
+
+        Menu open: "hidden", Menu closed: "block"
+        -->
+		<svg
+			class="size-6"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke-width="1.5"
+			stroke="currentColor"
+			aria-hidden="true"
+			data-slot="icon"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+			/>
+		</svg>
+	{/if}
+</button>
+
+<div class="relative">
+	{#if isOpen}
+		<div class="absolute left-1/2 z-10 mt-5 flex  -translate-x-1/2 px-4">
+			<div
+				class="flex-auto overflow-hidden 
+                 dark:ring-white/10 
+                 dark:bg-zinc-800/90 
+                 dark:text-zinc-200
+                 rounded-3xl bg-white text-sm shadow-lg ring-1 ring-gray-900/5"
+			>
+				<div class="p-4">
+					{#each itemsList as item}
+						<div class="group relative flex gap-x-6 rounded-lg p-4 
+                        dark:hover:bg-zinc-600 hover:bg-gray-50">
+							<div
+                                
+								class="{showIcon ? 'block' : 'hidden'} mt-1 flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50  group-hover:bg-white"
+							>
+								<svg
+									class="size-6 text-gray-600 group-hover:text-indigo-600"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+								>
+									<path stroke-linecap="round" stroke-linejoin="round" d={item.icon} />
+								</svg>
+							</div>
+							<div>
+								<a href={item.href} class="font-semibold 
+                                dark:text-zinc-200 text-gray-900">{item.title}</a>
+								<!-- <p class="mt-1 text-gray-600">{item.description}</p> -->
+							</div>
+						</div>
+					{/each}
+				</div>
+			</div>
+		</div>
+	{/if}
+</div>
--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,75 +1,51 @@
 <script lang="ts">
-	import NavItem from './core/NavItem.svelte';
-	import MobileNavigation from './MobileNavigation.svelte';
+import NavItem from './core/NavItem.svelte';
+import MobileNavigation, { type ItemList } from './MobileNavigation.svelte';
 
-	export let type: 'mobile' | 'desktop' = 'mobile';
-	let isOpen = false;
-  
-  function toggleMenu() {
+export let type: 'mobile' | 'desktop' = 'mobile';
+let itemsList: ItemList[] = [{
+    title: 'Home',
+    description: 'Get a bit panoramic of me',
+    icon: 'M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z',
+	href: '/'
+}, {
+    title: 'Eventi',
+    description: 'Speak directly to your customers',
+    icon: 'M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59',
+	href: '/content'
+}, {
+    title: 'Blog',
+    description: "Your customers' data will be safe and secure",
+    icon: 'M7.864 4.243A7.5 7.5 0 0 1 19.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 0 0 4.5 10.5a7.464 7.464 0 0 1-1.15 3.993m1.989 3.559A11.209 11.209 0 0 0 8.25 10.5a3.75 3.75 0 1 1 7.5 0c0 .527-.021 1.049-.064 1.565',
+	href: '/blog'
+},
+{
+    title: 'Resume',
+    description: "Your customers' data will be safe and secure",
+    icon: 'M7.864 4.243A7.5 7.5 0 0 1 19.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 0 0 4.5 10.5a7.464 7.464 0 0 1-1.15 3.993m1.989 3.559A11.209 11.209 0 0 0 8.25 10.5a3.75 3.75 0 1 1 7.5 0c0 .527-.021 1.049-.064 1.565',
+	href: '/blog'
+}
+]
+let isOpen = false;
+
+function toggleMenu() {
     isOpen = !isOpen;
-  }
+}
 </script>
 
 <nav>
-	{#if type == 'desktop'}
-		<ul
-			class="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
-		>
-			<NavItem href="/">Home</NavItem>
-			<NavItem href="/content">Eventi</NavItem>
-			<NavItem href="/blog">Blog</NavItem>
-			<NavItem href="/resume">Resume</NavItem>
-		</ul>
-	{:else}
-		<MobileNavigation></MobileNavigation>
-		<button
-			type="button"
-			class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-			aria-controls="mobile-menu"
-			aria-expanded="false"
-			on:click
-		>
-			<span class="absolute -inset-0.5"></span>
-			<span class="sr-only">Open main menu</span>
-			<!--
-              Icon when menu is closed.
-  
-              Menu open: "hidden", Menu closed: "block"
-            -->
-			<svg
-				class="block size-6"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				aria-hidden="true"
-				data-slot="icon"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-				/>
-			</svg>
-			<!--
-              Icon when menu is open.
-  
-              Menu open: "block", Menu closed: "hidden"
-            -->
-			<svg
-				class="hidden size-6"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				aria-hidden="true"
-				data-slot="icon"
-			>
-				<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-			</svg>
-		</button>
-		
-	{/if}
+    {#if type == 'desktop'}
+    <ul
+        class="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+        >
+        <NavItem href="/">Home</NavItem>
+        <NavItem href="/content">Eventi</NavItem>
+        <NavItem href="/blog">Blog</NavItem>
+        <NavItem href="/resume">Resume</NavItem>
+    </ul>
+    {:else}
+    <MobileNavigation itemsList={itemsList}></MobileNavigation>
 
-	
+    {/if}
+
 </nav>
