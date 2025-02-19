58e1b81808ff8e2c7ddb2ca2f47fd668294e63fc
scardummichele <scarpa.michele.90@gmail.com>
2025-02-19 16:54:32 +0100

    single article layour

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,14 +1,11 @@
 <script>
+	import ArticleLayout from '../../../components/blog/ArticleLayout.svelte';
 	import Container from '../../../components/Container.svelte';
 
 	export let data;
 	let { metadata, content } = data;
 </script>
 
-<Container className="mt-9">
-<article class="text-white">
-	<h1>{metadata.title}</h1>
-	<p>{metadata.date}</p>
+<ArticleLayout article={metadata}>
 	<svelte:component this={content} />
-</article>
-</Container>
+</ArticleLayout>
