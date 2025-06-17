288f37447377844e67dc324c00e761293507219d
scardummichele <scarpa.michele.90@gmail.com>
2025-06-17 20:48:24 +0200

    adapter vercel config

---
+++ configurations/svelte.static.config.js
@@ -0,0 +1,38 @@
+import { mdsvex } from 'mdsvex';
+import path from 'path';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	extensions: ['.svelte', '.md'],
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extension: '.md'
+		})
+	],
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter({
+			fallback: '200.html', //null
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			strict: true
+		}),
+		// prerender: {
+		// 	entries: ['*']
+		// },
+		alias: {
+            $content: path.resolve('./src/content')
+        }
+
+	}
+};
+
+export default config;
