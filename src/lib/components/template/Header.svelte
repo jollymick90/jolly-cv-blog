5792df9eec34b377092f5c5daf7a18ea0cb06cc7
JollyMick <scarpa.michele.90@gmail.com>
2025-06-25 19:56:08 +0200

    fix cv pdf printable

--- src/lib/components/template/Header.svelte
+++ src/lib/components/template/Header.svelte
@@ -2,22 +2,25 @@
 	import Avatar from '$lib/components/theme/Avatar.svelte';
 	import { onMount } from 'svelte';
 	import { page } from '$app/state';
-	
+
 	import Container from '$lib/components/template/Container.svelte';
 	import Navigation from '$lib/components/template/Navigation.svelte';
 	import ThemeToggle from '$lib/components/template/ThemeToggle.svelte';
 	import TailwindDetect from '../theme/TailwindDetect.svelte';
 	import type { LangAvailable } from '$lib/types';
 	import LangSelect from './LangSelect.svelte';
+	import ArrowDownIcon from '../icons/ArrowDownIcon.svelte';
+	import { ArrowDownCircle, Icon } from 'svelte-hero-icons';
 
-	const { lang }:{lang: LangAvailable} = $props();
+	const { lang }: { lang: LangAvailable } = $props();
 
 	let headerRef: HTMLDivElement | null = $state(null);
-	let avatarRef: HTMLDivElement | null = $state(null);;
+	let avatarRef: HTMLDivElement | null = $state(null);
 
 	let isInitial = true;
-	
+
 	let isHomePage = $derived(page.url.pathname === '/');
+	let isCVPage = $derived(page.url.pathname.includes('cv'));
 
 	function clamp(number: number, a: number, b: number) {
 		let min = Math.min(a, b);
@@ -34,16 +37,12 @@
 	}
 
 	function updateHeaderStyles() {
-		
 		if (!headerRef) return;
-		
+
 		const downDelay = avatarRef?.offsetTop ?? 0;
 		const upDelay = 64;
 		const { top, height } = headerRef.getBoundingClientRect();
-		const scrollY = clamp(
-			window.scrollY,
-			 0,
-			  document.body.scrollHeight - window.innerHeight);
+		const scrollY = clamp(window.scrollY, 0, document.body.scrollHeight - window.innerHeight);
 
 		if (isInitial) {
 			setProperty('--header-position', 'sticky');
@@ -89,9 +88,8 @@
 
 		let x = (scrollY * (fromX - toX)) / downDelay + toX;
 		x = clamp(x, fromX, toX);
-		
-		setProperty('--avatar-image-transform', 
-		`translate3d(${x}rem, 0, 0) scale(${scale})`);
+
+		setProperty('--avatar-image-transform', `translate3d(${x}rem, 0, 0) scale(${scale})`);
 
 		const borderScale = 1 / (toScale / scale);
 		const borderX = (-toX + x) * borderScale;
@@ -115,7 +113,18 @@
 			window.removeEventListener('resize', updateStyles);
 		};
 	});
+
+	function downloadCV() {
+		const url = `/${lang}_michele_scarpa_cv.pdf`;
+		const link = document.createElement('a');
+		link.href = url;
+		link.download = 'Michele-Scarpa-CV.pdf'; // nome del file salvato
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+	}
 </script>
+
 <TailwindDetect />
 
 <header
@@ -123,24 +132,20 @@
 	style="height: var(--header-height); margin-bottom: var(--header-mb);"
 >
 	{#if isHomePage}
-		
 		<div
 			bind:this={avatarRef}
 			class="order-last mt-[calc(theme(spacing.16)-theme(spacing.3))]"
 		></div>
-		<Container
-			className="top-0 order-last -mb-3 pt-3"
-			style="position: var(--header-position);"
-		>
+		<Container className="top-0 order-last -mb-3 pt-3" style="position: var(--header-position);">
 			<div
 				class="top-[var(--avatar-top,theme(spacing.3))] w-full"
 				style="position: var(--header-inner-position);"
 			>
 				<div class="relative">
 					<div
-						class="absolute left-0 top-3 origin-left transition-opacity
-						h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg
-						shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10"
+						class="absolute left-0 top-3 h-10 w-10
+						origin-left rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5
+						ring-1 ring-zinc-900/5 backdrop-blur transition-opacity dark:bg-zinc-800/90 dark:ring-white/10"
 						style="opacity: var(--avatar-border-opacity, 0); transform: var(--avatar-border-transform);"
 					></div>
 					<Avatar
@@ -148,7 +153,6 @@
 						className="block h-16 w-16 origin-left"
 						styleTransform="transform: var(--avatar-image-transform);"
 					></Avatar>
-					
 				</div>
 			</div>
 		</Container>
@@ -162,7 +166,7 @@
 				<div class="flex flex-1">
 					{#if !isHomePage}
 						<div
-						class="h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg
+							class="h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg
 						shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10"
 						>
 							<Avatar />
@@ -171,19 +175,31 @@
 				</div>
 				<div class="white flex flex-1 justify-end md:justify-center">
 					<div class="pointer-events-auto md:hidden">
-						<Navigation type='mobile' lang={lang}/>
+						<Navigation type="mobile" {lang} />
 					</div>
-					<div class="pointer-events-auto hidden md:block">					
-						<Navigation type='desktop' lang={lang}/>						
+					<div class="pointer-events-auto hidden md:block">
+						<Navigation type="desktop" {lang} />
 					</div>
-
 				</div>
 				<div class="flex justify-end md:flex-1">
-					<div class="flex pointer-events-auto">
+					<div class="pointer-events-auto flex">
+						{#if isCVPage}
+							<button
+							class="group inline-flex w-full items-center justify-center gap-2 rounded-full 
+							bg-zinc-50 px-3 py-3 mx-2 text-sm font-medium text-zinc-900 outline-offset-2 transition
+							 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 active:transition-none dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70"
+							aria-label="click"
+							onclick={() => downloadCV()}
+						>
+							
+							<Icon class="h-7 w-7 dark:text-white text-zinc-400"
+		 aria-hidden="true" src={ArrowDownCircle} />
+						</button>
+						{/if}
+						
 						<ThemeToggle />
 						<LangSelect />
 					</div>
-
 				</div>
 			</div>
 		</Container>
