d1bcd613672e3dca072400a5e3afb3510e14fe0b
scardummichele <scarpa.michele.90@gmail.com>
2025-06-30 00:06:03 +0200

    analytics vercel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import '../app.css';
-	import Header from '$lib/components/template/Header.svelte';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
+
 	import { langStore } from '$lib/i18n/lang.store';
+    import { dev } from '$app/environment';
+
+    // Inject the Analytics functionality
+    injectAnalytics({ mode: dev ? 'development' : 'production' });
+
 	let { children, data } = $props();
 	langStore.set(data.lang);
 	
