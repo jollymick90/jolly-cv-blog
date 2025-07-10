9324e0016348b4a881d3c9e48577d872d1e87c89
scardummichele <scarpa.michele.90@gmail.com>
2025-07-10 15:27:53 +0200

    add qr page link

---
+++ src/lib/components/utils/QRLink.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { qr } from '@svelte-put/qr/svg';
+    const { qrUrlLink } = $props();
+</script>
+
+<div class="w-full p-10 flex justify-center align-middle bg-white">
+<svg
+  use:qr={{
+    data: qrUrlLink,
+    // logo: 'https://svelte-put.vnphanquang.com/images/svelte-put-logo.svg',
+    shape: 'circle',
+  }}
+/>
+</div>
---
+++ src/routes/[lang]/qr/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Container from '$lib/components/template/Container.svelte';
+	import QrLink from '$lib/components/utils/QRLink.svelte';
+		
+</script>
+
+<svelte:head>
+	<title>QrLink</title>
+</svelte:head>
+
+<Container className="mt-9">
+	<QrLink qrUrlLink="https://michele-scarpa.vercel.app/"></QrLink>
+</Container>
