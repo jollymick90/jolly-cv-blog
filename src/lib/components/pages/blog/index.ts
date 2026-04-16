fbc78683795344f13bf1e2c4ae0f941e87e58258
jollymick90 <scarpa.michele.90@gmail.com>
2026-04-16 11:10:42 +0200

    feat(blog): wire new ArticleLayout, remove old zinc layout

--- src/lib/components/pages/blog/index.ts
+++ src/lib/components/pages/blog/index.ts
@@ -1,3 +1,2 @@
 // @index('./*.svelte', f => `export { default as ${f.name} } from '${f.path}.svelte';`)
 export { default as Article } from './Article.svelte';
-export { default as ArticleLayout } from './ArticleLayout.svelte';
--- src/routes/[lang]/blog/[slug]/+page.svelte
+++ src/routes/[lang]/blog/[slug]/+page.svelte
@@ -1,12 +1,16 @@
-<script>
-	import ArticleLayout from '$lib/components/pages/blog/ArticleLayout.svelte';
+<script lang="ts">
+  import ArticleLayout from '$lib/components/blog/ArticleLayout.svelte';
 
-	export let data;
-	let { metadata, content } = data;
-	
+  let { data } = $props();
+  const Content = $derived(data.content);
 </script>
 
-<ArticleLayout article={metadata}>
-	
-	<svelte:component this={content} />
+<ArticleLayout
+  metadata={data.metadata}
+  readingTime={data.readingTime}
+  lang={data.lang}
+  prev={data.prev}
+  next={data.next}
+>
+  <Content />
 </ArticleLayout>
